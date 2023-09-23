1398a001b046208bde245ebaf64512a9e91c8b50
Christian Basile <basile.christian94@gmail.com>
2023-09-22 21:02:10 -0400

    fix bosslog script to pull missing bosses from wiki; and standardize the boss id

--- src/page/DropLog.tsx
+++ src/page/DropLog.tsx
@@ -23,8 +23,9 @@ To obtain the list of all drop logs for droplog.json, do the following:
 1. go to this url:
       https://runescape.wiki/w/Boss_collection_log
 2. run this command in the console:
+      const bossNames = ['Araxxi', 'Arch-Glacor', 'Barrows', 'Rise of the Six', 'Chaos Elemental', 'Commander Zilyana', 'Corporeal Beast', 'Croesus', 'Dagannoth Kings', 'Dragonkin Laboratory', 'General Graardor', 'Giant Mole', 'Gregorovic', 'Helwyr', 'Hermod, the Spirit of War', 'Kalphite King', 'Kalphite Queen', 'Kerapac', 'King Black Dragon', "Kree'arra", "K'ril Tsutsaroth", 'Legiones', 'Liberation of Mazcab', 'The Magister', 'Nex', 'Nex: Angel of Death', 'Queen Black Dragon', 'Raksha', 'Rasial, the First Necromancer', 'Rex Matriarchs', 'The Shadow Reef', 'Solak', 'Telos', 'Temple of Aminishi', 'Twin Furies', 'TzHaar', 'TzKal-Zuk', 'Vindicta & Gorvek', 'Vorago', 'Zamorak, Lord of Chaos']
       const bossImgByName = {}
-      const bossNameMapper = {'Kerapac, the bound': 'Kerapac', 'Giant mole':'Giant Mole', 'The Barrows Brothers': 'Barrows'}
+      const bossNameMapper = {'Kerapac, the bound': 'Kerapac', 'Giant mole':'Giant Mole', 'The Barrows Brothers': 'Barrows', 'The Barrows: Rise of the Six': 'Rise of the Six'}
       const bossTable = document.getElementsByClassName('wikitable')[0]
       bossTable.getElementsByTagName('tbody')[0].getElementsByTagName('tr').forEach(row => {
           const firstCell = row.getElementsByTagName('td')[0]
@@ -35,21 +36,26 @@ To obtain the list of all drop logs for droplog.json, do the following:
           }
       })
       const bosses = []
-      document.getElementsByClassName('boss-collection-log').forEach(collectionLogTable => {
-          const bossName = collectionLogTable.previousElementSibling.previousElementSibling.getElementsByClassName('mw-headline')[0].innerText
-          const bossConfig = {name: bossName, id: bosses.length+1, drops: [], imgSrc: bossImgByName[bossName]}
-          collectionLogTable.getElementsByTagName('td').forEach(itemCell => {
-              const firstAnchorInItemCell = itemCell.getElementsByTagName('a')[0]
-              if(firstAnchorInItemCell != undefined){
-                  const item = firstAnchorInItemCell.title
-                  const anchorLink = firstAnchorInItemCell.href
-                  const imgTag = firstAnchorInItemCell.getElementsByTagName('img')[0]
-                  const imgTagSrc = imgTag.src.split('?')[0]
-                  bossConfig.drops.push({id: bossConfig.drops.length+1, name: item, imgSrc: imgTagSrc, link: anchorLink})
+      document.getElementsByClassName('wikitable').forEach((collectionLogTable, idx) => {
+          if(idx > 0 && idx < 41) {
+              const bossName = collectionLogTable.previousElementSibling.previousElementSibling.getElementsByClassName('mw-headline')[0].innerText
+              const bossNameIdx = bossNames.indexOf(bossName)+1
+              if(bossNameIdx == -1) {
+                  console.warn(`Boss "${bossName}" was not on the list.  Add it to the end of the list to index the boss correctly`);
               }
-          })
-          bosses.push(bossConfig)
-          
+              const bossConfig = {name: bossName, id: bossNameIdx, drops: [], imgSrc: bossImgByName[bossName]}
+              collectionLogTable.getElementsByTagName('td').forEach(itemCell => {
+                  const firstAnchorInItemCell = itemCell.getElementsByTagName('a')[0]
+                  if(firstAnchorInItemCell != undefined){
+                      const item = firstAnchorInItemCell.title
+                      const anchorLink = firstAnchorInItemCell.href
+                      const imgTag = firstAnchorInItemCell.getElementsByTagName('img')[0]
+                      const imgTagSrc = imgTag.src.split('?')[0]
+                      bossConfig.drops.push({id: bossConfig.drops.length+1, name: item, imgSrc: imgTagSrc, link: anchorLink})
+                  }
+              })
+              bosses.push(bossConfig)
+          }
       })
       JSON.stringify(bosses)
 3. copy the output and paste it into droplog.json (remove leading/trailing quotes)
