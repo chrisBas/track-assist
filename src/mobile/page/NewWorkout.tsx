b322a5baf55fea758e0ee805afa7c1902358c1c8
Christian Basile <basile.christian94@gmail.com>
2024-03-26 21:36:34 -0400

    add search menu to workout select

--- src/mobile/page/NewWorkout.tsx
+++ src/mobile/page/NewWorkout.tsx
@@ -1,19 +1,33 @@
-import { ChevronLeft, FitnessCenter } from "@mui/icons-material";
+import { ChevronLeft, FitnessCenter, Search } from "@mui/icons-material";
 import {
   Box,
+  Divider,
   IconButton,
+  InputBase,
   List,
+  ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Paper,
 } from "@mui/material";
+import { useState } from "react";
 import useActiveApp from "../../hook/useActiveApp";
 import { useExercise } from "../../hook/useExercise";
 import TopAppBar from "../component/TopAppBar";
 
 export default function NewWorkout() {
+  // global state
   const { goBack } = useActiveApp();
   const { items: exercises } = useExercise();
+
+  // local state
+  const [search, setSearch] = useState("");
+
+  // local vars
+  const filteredExercises = exercises.filter((exercise) => {
+    return exercise.exercise.toLowerCase().includes(search.toLowerCase());
+  });
 
   return (
     <>
@@ -26,8 +40,30 @@ export default function NewWorkout() {
       />
       <Box>
         <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-          {exercises.map((exercise, idx) => {
-            // TODO: add search to navbar
+          <ListItem>
+            <Paper
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                width: 400,
+              }}
+            >
+              <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+                <Search />
+              </IconButton>
+              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search"
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+            </Paper>
+          </ListItem>
+          {filteredExercises.map((exercise, idx) => {
             return (
               <ListItemButton
                 key={exercise.id}
