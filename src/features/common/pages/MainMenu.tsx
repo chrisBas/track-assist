926ea67a4d8cb3688425989a2103e1206f62997e
Christian Basile <basile.christian94@gmail.com>
2024-04-05 23:31:47 -0400

    refresh main menu look

--- src/features/common/pages/MainMenu.tsx
+++ src/features/common/pages/MainMenu.tsx
@@ -8,49 +8,50 @@ import {
   Typography,
 } from "@mui/material";
 import { apps } from "../../../App";
+import TopAppBar from "../components/TopAppBar";
 import useActiveApp from "../hooks/useActiveApp";
 
 export default function MainMenu() {
   const { setActiveApp } = useActiveApp();
 
   return (
-    <Box p={1}>
-      <Typography gutterBottom variant="h5" component="div">
-        Applets
-      </Typography>
-      <Grid container spacing={2}>
-        {apps.map((app) => (
-          <Grid item key={app.name} xs={12}>
-            <Card sx={{borderRadius: 3}}>
-              <CardActionArea
-                onClick={() => {
-                  setActiveApp(() => ({
-                    app: app.name,
-                    nav: app.nav[0].label,
-                    page: app.nav[0].pages[0].label,
-                  }));
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  height="120"
-                  image={app.img}
-                  alt={app.name}
-                  sx={{ objectFit: "contain" }}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {app.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {app.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      <TopAppBar title="Applets" showProfile />
+      <Box sx={{ flexGrow: 1, overflow: "scroll" }}>
+        <Grid container spacing={2}>
+          {apps.map((app) => (
+            <Grid item key={app.name} xs={12}>
+              <Card sx={{ borderRadius: 3 }}>
+                <CardActionArea
+                  sx={{ display: "flex" }}
+                  onClick={() => {
+                    setActiveApp(() => ({
+                      app: app.name,
+                      nav: app.nav[0].label,
+                      page: app.nav[0].pages[0].label,
+                    }));
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    image={app.img}
+                    alt={app.name}
+                    sx={{ objectFit: "contain", width: "120px" }}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {app.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {app.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Box>
   );
 }
